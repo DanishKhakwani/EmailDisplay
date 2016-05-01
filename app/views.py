ac19e780a0b7177d838a10beca52744852f64dda
Danish Khakwani <danish.khakwani@yahoo.com>
2016-05-01 00:22:25 -0700

    index and detail view implemented, Emails can be searched by providing senderId
    form is still required at index page, hardcoded value has been set at index view for now

--- app/views.py
+++ app/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.template import loader
+
+from .models import Email, User
 
 # Create your views here.
+
+def index(request):
+    emails = Email.objects.filter(senderId="P8561044896590126190")
+    context = {
+            'emails': emails,
+    }
+
+    return render(request, 'app/index.html', context)
+
+def detail(request, email_id):
+    print(email_id)
+    email = Email.objects.get(pk=email_id)
+    sender = User.objects.get(pk=email.senderId)
+    print(type(email))
+    context = {
+            'email': email,
+            'sender': sender,
+    }
+    return render(request, 'app/detail.html', context)
